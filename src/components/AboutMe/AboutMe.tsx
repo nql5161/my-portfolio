e6b644c0a76cb6e72d47453c6aac477c31720134
Nikolas Lecce <34181603+nql5161@users.noreply.github.com>
2021-01-23 11:23:53 -0500

    Added styles and components
    Added components and a little bit of styling for those components.

---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './AboutMe.css';
+
+export const AboutMe = () => {
+    return (
+        <div>
+            <h2 className={"header"}>Hi, my name is Nikolas Lecce</h2>
+        </div>
+    )
+}
---
+++ src/screens/AboutMeScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {AboutMe} from "../components/AboutMe/AboutMe";
+
+export const AboutMeScreen = () => {
+    return (
+        <AboutMe/>
+    )
+}
